1e01a953f84be642673c7844e7b9105e5d1ff9a0
Zylvian <jovlisen@gmail.com>
2019-03-28 04:12:39 +0100

    Added improvements to OWS

--- onewordstory/onewordstory.py
+++ onewordstory/onewordstory.py
@@ -1,6 +1,6 @@
 # Discord 
 import discord
-from redbot.core import commands
+
 import random
 import time
 import unicodedata
@@ -16,6 +16,10 @@ import time
 # Red
 from redbot.core import Config, bank, commands, checks
 from redbot.core.data_manager import bundled_data_path
+from redbot.core import commands
+from redbot.core.utils.predicates import MessagePredicate
+from redbot.core.utils.chat_formatting import humanize_list
+
 
 
 # Standard Library
@@ -28,10 +32,12 @@ import uuid
 from bisect import bisect
 from copy import deepcopy
 from itertools import zip_longest
+import json
 
 
 # Red
 from redbot.core import Config, bank, commands, checks
+from redbot.core.data_manager import bundled_data_path
 
 # Discord 
 import discord
@@ -49,7 +55,7 @@ class OneWordStory(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
         self.config = Config.get_conf(self, 420420420, force_registration=True)
-        
+        self.path = bundled_data_path(self)
      
      
         ows_defaults = {'Cooldown': 3600,
@@ -58,8 +64,9 @@ class OneWordStory(commands.Cog):
                             'Start_time': 60, # Change me to 60
                             'Answer_time': 14,
                             'Max_words': 40,
-                            'User_time_add': 20,
-                            'Gallery_channel_id': None
+                            'User_time_add': 10,
+                            'Gallery_channel_id': None,
+                            'Startup_lines':['General'],
                             }
                             
         # If releasing, make sure to change the channel ID. Can be an available command with e.g set_channel
@@ -72,18 +79,98 @@ class OneWordStory(commands.Cog):
         """Ows group command"""
         pass
 
-    @ows.command()
+    @ows.group(autohelp=True)
+    async def gallery(self, ctx):
+        """Gallery settings"""
+        pass
+
+    @ows.group(autohelp=True)
+    async def settings(self, ctx):
+        """General OWS settings"""
+        pass
+
+    @settings.group(autohelp=True)
+    async def lines(self, ctx):
+        """Pick which lines to use with your bot!"""
+        pass
+
+    @settings.command()
     async def counter_reset(self,ctx):
         """Resets the game counter!"""
         async with self.config.guild(ctx.guild).Counter() as counter:
             counter = 0
             await ctx.send("Counter reset to " + counter)
+            
+    @lines.command()
+    async def add(self,ctx):
 
-    @ows.group(autohelp=True)
-    async def gallery(self, ctx):
-        """Gallery settings"""
-        pass
-    
+        self.gconf = self.config.guild(ctx.guild)
+
+        current_categories = await self.gconf.get_raw("Startup_lines")
+        default_json_lines_dict = await self.get_default_lines(ctx)
+        default_json_categories = list(default_json_lines_dict["Startup_lines"])
+
+
+        available_list = [category for category in default_json_categories if category not in current_categories]
+        list_string = ""
+        for i, item in enumerate(available_list):
+            list_string += "\n**{}**. {}".format(i+1, item)
+        await ctx.send("**Current startup lines**: {} \n**All available**: {}\n*Which one do you want to add?*"
+                       .format(humanize_list(current_categories),
+                       list_string))
+
+        try:
+            while True:
+                pred = MessagePredicate.valid_int(ctx)
+                await ctx.bot.wait_for('message',timeout=7,check=pred)
+                number_choice = pred.result-1 # Minus one due to 0-indexed
+                try:
+                    add_category = available_list[number_choice]
+                    #add_index = default_json_categories.index(add_category)
+                    async with self.gconf.Startup_lines() as startup_lines:
+                        startup_lines.append(add_category)
+                        await ctx.send("Category added!")
+                except IndexError:
+                    return await ctx.send("Incorrect number!")
+        except asyncio.TimeoutError:
+            await ctx.send("Timed out!")
+
+
+
+    @lines.command()
+    async def rem(self, ctx):
+        self.gconf = self.config.guild(ctx.guild)
+        current_categories = await self.gconf.get_raw("Startup_lines")
+        list_string = ""
+        for i, item in enumerate(current_categories):
+            list_string += "\n**{}**. {}".format(i+1, item)
+        await ctx.send("**Current startup lines**: {} \n*Which one do you want to remove?*"
+                       .format(list_string))
+
+        try:
+            while True:
+                pred = MessagePredicate.valid_int(ctx)
+                await ctx.bot.wait_for('message',timeout=7,check=pred)
+                number_choice = pred.result-1 # Minus one due to 0-indexed
+                try:
+                    rem_category = current_categories[number_choice]
+                    #add_index = default_json_categories.index(add_category)
+                    async with self.gconf.Startup_lines() as startup_lines:
+                        startup_lines.remove(rem_category)
+                        return await ctx.send("Category removed!")
+                except IndexError:
+                    await ctx.send("Incorrect number!")
+        except asyncio.TimeoutError:
+            await ctx.send("Timed out!")
+
+
+    async def get_default_lines(self, ctx):
+        filepath = self.path / 'default_lines.json'
+        with open(filepath) as json_file:
+            return(json.load(json_file))
+
+
+
     @gallery.command()
     async def set_channel(self, ctx):
         """Set the gallery channel!"""
@@ -102,7 +189,7 @@ class OneWordStory(commands.Cog):
     async def start(self, ctx):
         """Starts a game of One Word Story!"""
 
-        ows_values = {
+        """ows_values = {
                     "Games":{
                         "One Word Story":
                             {},
@@ -110,22 +197,22 @@ class OneWordStory(commands.Cog):
                             "Start": "It's time to make a song!"
                             }
                         }
-                    }
-    
-        startup_lines = ["Did you hear of the...", "There once was a...", '"Morty, we gotta...',
-                        "The old man from...","Somebody once told me...", "The universe is...",
-                        "The fact of the matter is...", "Did you know that...", "Fyre makes soap out of...",
-                        "The FBI doesnt know yet but...", "After a long talk my roommates and I decided that...",
-                        "My father used to always say...",  "Your mother is...",
-                        "Have you ever wondered about...", "Sorry I was late boss, I...", "Mom, it's not what it looks like. I was just...",
-                        "One thing that people dont understand is...", "As a licensed professional I think that...",
-                        "\"What are you doing?\" Don't worry I was just...", "When you look at it from a technical standpoint...", 
-                        "Did you know that its illegal to...", "I watch Rick and Morty because..."]
-        sad_lines = ["Oh. Well, I uh, I had better things to do anyways! Like uh, do things, and stuff! *By myself...*", "Hello? Nobody? No...?",
-                     "Play with me, damnit! I refuse to go back to the butter-passing factory!", "Oh nobody? Bah, I guess you are all busy." \
-                     " Or sick. Or dead. *Hopefully dead...*","**ECHO**,**echo**, echo, *echo*...",
-                     "What music do I listen to? Good question, human who is actually my friend and actually exists. Thanks"\
-                     " for not leaving me hanging here!  \n\n :'("]
+                    }"""
+        self.gconf = self.config.guild(ctx.guild)
+        current_categories = await self.gconf.get_raw("Startup_lines")
+        filepath = self.path / 'default_lines.json'
+        startup_lines = list()
+        bonus_round_time = 0
+
+        with open(filepath) as json_file:  
+            data = json.load(json_file)
+
+            for name, line_list in data["Startup_lines"].items():
+                if name in current_categories:
+                    startup_lines.extend(line_list)
+
+            sad_lines = data["Sad_lines"]
+        
         
         #game_name = random.choice(ows_values.keys())
         game_name = "One Word Story"
@@ -159,9 +246,10 @@ class OneWordStory(commands.Cog):
                 message = await self.bot.wait_for('message',    
                                               timeout=(start_time - (current-begin).seconds),check=usercheck
                                               )
-                if message.author not in join_users and message.content.lower() == 'ows' and message.author != self.bot.user:
-                    join_users.append(message.author)
-                    await ctx.send("{} joined! {} seconds added to the game clock!".format(message.author.mention, user_time_add))
+                (join_users, join_bool) = await self.join_user_add(ctx, message, join_users)
+                if join_bool:
+                    bonus_round_time += user_time_add * len(join_users)
+                    await ctx.send("+{} 🕒 total seconds have been added to the game clock!".format(bonus_round_time))
                 
                 # await message.delete()
                 
@@ -184,6 +272,7 @@ class OneWordStory(commands.Cog):
         #start_line = start_line.strip('"')
         
         # Takes user input on a cycle.
+        ## INPUT PLACE
         start_line = await self.take_input(ctx, join_users, start_line)
 
         start_line += "."
@@ -235,9 +324,15 @@ class OneWordStory(commands.Cog):
         await self.gconf.set_raw(game_name, counter, "Participants",value=participants)
         await self.gconf.set_raw(game_name, counter, "Timestamp",value=int(time.time()))
 
+    async def join_user_add(self, ctx, message:discord.Message, join_users:list):
+        if(message.author not in join_users and message.content.lower()=="ows"):
+                join_users.append(message.author)
+                await ctx.send("{} joined!".format(message.author.mention))
+                return (join_users, True)
+        else:
+            return (join_users, False) 
 
-
-    async def take_input(self, ctx, join_users, start_line):
+    async def take_input(self, ctx, join_users, start_line, bonus_round_time:int):
 
         def usercheck(message):
             return message.author != self.bot.user and message.channel.id == ctx.channel.id
@@ -248,7 +343,7 @@ class OneWordStory(commands.Cog):
         timeout_value = await self.config.guild(ctx.guild).get_raw('Round_time')
         # Adds time to the clock when users join.
         user_time_add = await self.config.guild(ctx.guild).get_raw('User_time_add')
-        timeout_value += user_time_add*len(join_users)
+
         user_cd = await self.config.guild(ctx.guild).get_raw('Answer_time')
         all_users = join_users[:] # Optionally join_users.copy()
         cd_users = list()
@@ -282,11 +377,11 @@ class OneWordStory(commands.Cog):
                 wordmsg = await ctx.send("*{}*...\nAlright {}, give me a word! *{} seconds remaining...*".format(start_line, tempuser.mention, current))
                 
                 
-
                 # User timer is a set number, but if the overall cooldown is below the user timer, then that is the new timeout value.
                 if current < user_cd: 
                     user_cd = current
                 
+                # Checks the input messages
                 while True:
                     message = await self.bot.wait_for('message',
                                                   timeout=user_cd, check=usercheck)
@@ -304,6 +399,9 @@ class OneWordStory(commands.Cog):
                                 await ctx.send("Word too long!")
                         else:
                             await ctx.send("Only one word!")
+                    # Any other people typing
+                    else:
+                        (join_users, join_bool) = self.join_user_add(ctx, message, join_users)
                     
             # Either stops the game or goes to the next user.
             except asyncio.TimeoutError:
