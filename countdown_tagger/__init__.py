a7757c21af798fb8504890c73f96c1d62479d0ca
Zylvian <jovlisen@gmail.com>
2019-05-26 19:48:38 +0200

    Fixed countdown_tagger

--- countdown_tagger/__init__.py
+++ countdown_tagger/__init__.py
@@ -1,4 +1,8 @@
+
+from redbot.core import commands
 from .countdown_tagger import Countdown_Tagger
 
-def setup(bot):
-    bot.add_cog(Countdown_Tagger())
+def setup(bot: commands.Bot):
+
+    cog = Countdown_Tagger(bot)
+    bot.add_cog(cog)
--- countdown_tagger/countdown_tagger.py
+++ countdown_tagger/countdown_tagger.py
@@ -1,4 +1,6 @@
 from redbot.core import commands, Config, checks
+import datetime
+import dateutil.parser
 import discord
 import asyncio
 
@@ -8,17 +10,26 @@ import asyncio
 
 class Countdown_Tagger(commands.Cog):
 
-    def __init__(self):
+    def __init__(self, bot):
+        self.bot = bot
         self.database = Config.get_conf(self, identifier=420420420, force_registration=True)
+        self.premiere_date = "2019-11-01T00:00:00+0100"
         #self.database.register_guild(**defaults)
 
-    """async def on_message(self, message):
-        async with self.database.guild(message.guild).Only_Image_Channels() as only_image_channels:
-            if message.channel.id in only_image_channels:
-                if len(message.attachments) < 1:
-                    await message.delete()"""
 
     async def on_message(self, message):
-        if message.author.id == 233669548673335296:
-            message.channel.send("ayo")
+        if self.bot.user.mentioned_in(message):
+            premiere_time = dateutil.parser.parse(self.premiere_date).replace(tzinfo=None)
+            curr_time = datetime.datetime.now().replace(tzinfo=None)
+            days_til_premiere = (premiere_time-curr_time).days
+
+            send_msg = "Season 4 will premiere in **{}** days!".format(days_til_premiere)
+            
+            if days_til_premiere <= 0:
+                send_msg = "Season 4 has already premiered!"
+
+            await message.channel.send(send_msg)
+
+        #if message.author.id == 233669548673335296:
+        #    await message.channel.send("ayo")
 
