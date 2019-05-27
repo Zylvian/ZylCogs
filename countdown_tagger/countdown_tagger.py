c7278dd8086a438ae13b297057baeac6774a9a50
Zylvian <jovlisen@gmail.com>
2019-05-28 01:52:13 +0200

    Added custom message

--- countdown_tagger/countdown_tagger.py
+++ countdown_tagger/countdown_tagger.py
@@ -7,7 +7,8 @@ import asyncio
 from redbot.core.utils.predicates import MessagePredicate
 
 defaults = {"toggled": False,
-            "msg_format": "Season 4 will premiere in **{}** days!",
+            "custom_msg":"Season 4 will premiere in",
+            "msg_format": "{} **{}** days!",
             "premiere_date": None}
 
 
@@ -30,20 +31,7 @@ class Countdown_Tagger(commands.Cog):
         if toggled:
             if message.guild.me.mentioned_in(message):
 
-                msg_format = await gconf.msg_format()
-                premiere_date = await gconf.premiere_date()
-
-                if premiere_date is None:
-                    return
-
-                premiere_date = dateutil.parser.parse(premiere_date).replace(tzinfo=None)
-                curr_time = datetime.datetime.now().replace(tzinfo=None)
-                days_til_something = (premiere_date - curr_time).days
-
-                send_msg = msg_format.format(days_til_something)
-
-                if days_til_something <= 0:
-                    send_msg = "Season 4 has already premiered!"
+                send_msg = await self.get_send_msg(self, gconf)
 
                 await message.channel.send(send_msg)
 
@@ -58,8 +46,8 @@ class Countdown_Tagger(commands.Cog):
         """Set the date of your countdown!"""
         gconf = self.config.guild(ctx.guild)
 
-        await ctx.send("Post your date in this format: **DAY MONTH YEAR**\n"
-                       'I.e *17 04 1980* ')
+        await ctx.send("**Post your date in this format:** ***DAY MONTH YEAR***\n"
+                       '*I.e* `17 04 1980` ')
 
         usercheck = MessagePredicate.same_context(ctx)
 
@@ -69,7 +57,7 @@ class Countdown_Tagger(commands.Cog):
 
             new_date = dateutil.parser.parse(message.content)
 
-            await ctx.send("Is this the correct date?\n{}\n(y/n)".format(new_date))
+            await ctx.send("**Is this the correct date?**\n{}\n(y/n)".format(new_date.strftime("%d %m %y")))
 
             message = await self.bot.wait_for('message',
                                               timeout=15, check=usercheck) # type: discord.Message
@@ -98,3 +86,41 @@ class Countdown_Tagger(commands.Cog):
         gconf = self.config.guild(ctx.guild)
         await gconf.toggled.set(False)
         await ctx.send("Toggled the cooldown tagger off!")
+
+    @cd_tag.command()
+    async def message(self, ctx):
+        """Set the response message!"""
+        gconf = self.config.guild(ctx.guild)
+        await ctx.send("**Set your message:**\nFormat: * _________ x days!*")
+
+        usercheck = MessagePredicate.same_context(ctx)
+
+        try:
+            message = await self.bot.wait_for('message',
+                                          timeout=15, check=usercheck)
+
+            send_msg = await self.get_send_msg(self, gconf)
+            await ctx.send('Message set!\n"{}'.format(send_msg))
+
+        except asyncio.TimeoutError:
+            await ctx.send("Timed out!")
+
+
+
+    async def get_send_msg(self, gconf):
+
+        custom_msg = await gconf.msg_format()
+        msg_format = await gconf.msg_format()
+        premiere_date = await gconf.premiere_date()
+
+        if premiere_date is None:
+            return
+
+        premiere_date = dateutil.parser.parse(premiere_date).replace(tzinfo=None)
+        curr_time = datetime.datetime.now().replace(tzinfo=None)
+        days_til_something = (premiere_date - curr_time).days
+
+        send_msg = msg_format.format(custom_msg, days_til_something)
+
+        if days_til_something <= 0:
+            send_msg = "Season 4 has already premiered!"
