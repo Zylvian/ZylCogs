66d1da5f3bdd04a99976a2ca249e9fb127e8c4aa
Zylvian <jovlisen@gmail.com>
2019-09-10 05:15:58 +0200

    Tested

--- clapify/clapify.py
+++ clapify/clapify.py
@@ -1,3 +1,5 @@
+import json
+
 from redbot.core import commands, Config, checks
 import discord
 from typing import Union, Optional
@@ -10,7 +12,7 @@ class Clapify(commands.Cog):
         self.config = Config.get_conf(self, identifier=420420420, force_registration=True)
 
     @commands.command(autohelp=True)
-    async def clapify(self, ctx, emoji: Optional[discord.Emoji], *, var: str):#var: Union[discord.Message, str]):
+    async def clapify(self, ctx, emoji: Optional[discord.Emoji], *var: Union[discord.Message, str]):
         """Give me a string or a message ID!"""
         #
         if not emoji:
@@ -22,5 +24,10 @@ class Clapify(commands.Cog):
         clapified_str = var.replace(" ", " {} ".format(emoji))
 
         await ctx.send(clapified_str)
+
+    @commands.command(autohelp=True)
+    async def testballs(self, ctx, butt:discord.Message):
+
+        await ctx.send(butt)
 
 
