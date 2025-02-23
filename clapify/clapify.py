7c8f87aaf7f69a852466c322c099281e1879c7db
Jarle Wallevik <33404765+Zylvian@users.noreply.github.com>
2025-02-23 03:05:54 +0100

    set clapify to be tier 1 command

--- clapify/clapify.py
+++ clapify/clapify.py
@@ -1,8 +1,8 @@
 import json
 
+import discord
 from redbot.core import commands, Config, app_commands
 from redbot.core.utils import chat_formatting
-import discord
 from typing import Union, Optional
 
 
@@ -13,8 +13,8 @@ class Clapify(commands.Cog):
         self.config = Config.get_conf(self, identifier=420420420, force_registration=True)
 
     @commands.bot_has_permissions(manage_messages=True)
-    @app_commands.command()
-    async def clapify(self, ctx, emoji: Optional[discord.Emoji], *, var: Union[int, str]):
+    @commands.command()
+    async def clapify(self, ctx, emoji: Optional[discord.Emoji], *, message_id_or_string: Union[int, str]):
         """Give me a string or a message ID!"""
         #
         user_name = ctx.author.display_name
@@ -22,18 +22,15 @@ class Clapify(commands.Cog):
         if not emoji:
             emoji = "👏"
 
-        if var:
-            if not isinstance(var, str):
+        if message_id_or_string:
+            if not isinstance(message_id_or_string, str):
                 channel = ctx.channel
-                msg = await channel.fetch_message(var)
-                var = msg.content
+                msg = await channel.fetch_message(message_id_or_string)
+                message_id_or_string = msg.content
 
-
-
-        clapified_str = var.replace(" ", " {} ".format(emoji))
+        clapified_str = message_id_or_string.replace(" ", " {} ".format(emoji))
         clapified_pages = list(chat_formatting.pagify(clapified_str))
         clapified_pages[-1] += ("\n*(clapified by {})*".format(user_name))
-
 
         await ctx.message.delete()
 
@@ -41,10 +38,8 @@ class Clapify(commands.Cog):
             await ctx.send(page)
 
     @commands.command(autohelp=True)
-    async def testballs(self, ctx, butt:int):
+    async def testballs(self, ctx, butt: int):
         channel = ctx.channel
         msg = await channel.fetch_message(butt)
         msg = msg.content
         await ctx.send(msg)
-
-
